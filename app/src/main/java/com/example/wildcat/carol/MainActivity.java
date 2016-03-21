5dd0da2333107b8fda0144dada775e90d5ae75c6
mellenwo <mellenwo@villanova.edu>
2016-03-21 15:11:23 -0400

    updated mainActivity

--- app/src/main/java/com/example/wildcat/carol/MainActivity.java
+++ app/src/main/java/com/example/wildcat/carol/MainActivity.java
@@ -80,17 +80,17 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_house) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_chores) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_groceries) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_messages) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.nav_progres) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_settings) {
 
         }
 
